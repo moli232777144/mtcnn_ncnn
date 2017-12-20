5b1cf2c979bafac02858ffc7f46a39d10c3b4ccd
moli232777144 <232777144@qq.com>
2017-12-20 23:44:40 +0800

    添加源码目录

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include "mtcnn.h"
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+void test_video() {
+	char *model_path = "../models";
+	MTCNN mtcnn(model_path);
+	mtcnn.SetMinFace(40);
+	cv::VideoCapture mVideoCapture(0);
+	if (!mVideoCapture.isOpened()) {
+		return;
+	}
+	cv::Mat frame;
+	mVideoCapture >> frame;
+	while (!frame.empty()) {
+		mVideoCapture >> frame;
+		if (frame.empty()) {
+			break;
+		}
+
+		clock_t start_time = clock();
+		
+		ncnn::Mat ncnn_img = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
+		std::vector<Bbox> finalBbox;
+		mtcnn.detect(ncnn_img, finalBbox);
+		const int num_box = finalBbox.size();
+		std::vector<cv::Rect> bbox;
+		bbox.resize(num_box);
+		for(int i = 0; i < num_box; i++){
+			bbox[i] = cv::Rect(finalBbox[i].x1, finalBbox[i].y1, finalBbox[i].x2 - finalBbox[i].x1 + 1, finalBbox[i].y2 - finalBbox[i].y1 + 1);
+		 }
+		for (vector<cv::Rect>::iterator it = bbox.begin(); it != bbox.end(); it++) {
+			rectangle(frame, (*it), Scalar(0, 0, 255), 2, 8, 0);
+		}
+		imshow("face_detection", frame);
+		clock_t finish_time = clock();
+		double total_time = (double)(finish_time - start_time) / CLOCKS_PER_SEC;
+		std::cout << "time" << total_time * 1000 << "ms" << std::endl;
+	
+		int q = cv::waitKey(10);
+		if (q == 27) {
+			break;
+		}
+	}
+	return ;
+}
+
+int test_picture(){
+	char *model_path = "../models";
+	MTCNN mtcnn(model_path);
+
+	clock_t start_time = clock();
+
+	cv::Mat image;
+	image = cv::imread("../sample.jpg");
+	ncnn::Mat ncnn_img = ncnn::Mat::from_pixels(image.data, ncnn::Mat::PIXEL_BGR2RGB, image.cols, image.rows);
+	std::vector<Bbox> finalBbox;
+	mtcnn.detect(ncnn_img, finalBbox);
+
+	const int num_box = finalBbox.size();
+	std::vector<cv::Rect> bbox;
+	bbox.resize(num_box);
+	for (int i = 0; i < num_box; i++) {
+		bbox[i] = cv::Rect(finalBbox[i].x1, finalBbox[i].y1, finalBbox[i].x2 - finalBbox[i].x1 + 1, finalBbox[i].y2 - finalBbox[i].y1 + 1);
+		
+	}
+	for (vector<cv::Rect>::iterator it = bbox.begin(); it != bbox.end(); it++) {
+		rectangle(image, (*it), Scalar(0, 0, 255), 2, 8, 0);
+	}
+
+	imshow("face_detection", image);
+	clock_t finish_time = clock();
+	double total_time = (double)(finish_time - start_time) / CLOCKS_PER_SEC;
+	std::cout << "time" << total_time * 1000 << "ms" << std::endl;
+
+	cv::waitKey(0);
+
+}
+
+int main(int argc, char** argv) {
+	
+	test_video();
+	//test_picture();
+	return 0;
+}
